3a3c4afbf29e88150b7759dbcd9d58cdbed99e00
YeonSooYeon <dustndus8@naver.com>
2022-02-25 03:32:46 +0900

    refactor: main activity databinding

--- app/src/main/java/org/example/todolistapplication/MainActivity.kt
+++ app/src/main/java/org/example/todolistapplication/MainActivity.kt
@@ -2,29 +2,26 @@ package org.example.todolistapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import org.example.todolist.AfterFragment
 import org.example.todolist.BeforeFragment
 import org.example.todolist.MiddleFragment
+import org.example.todolistapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val tabs = findViewById<TabLayout>(R.id.tabs)
-        val viewPager = findViewById<ViewPager2>(R.id.viewpager)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val pagerAdapter = PagerAdapter(this)
-
         val tabName = listOf("시작 전", "진행 중", "완료")
 
-        viewPager.adapter = pagerAdapter
-        TabLayoutMediator(tabs, viewPager) { tab, position ->
+        binding.viewpager.adapter = pagerAdapter
+        TabLayoutMediator(binding.tabs, binding.viewpager) { tab, position ->
             tab.text = "${(tabName[position])}"
         }.attach()
     }
